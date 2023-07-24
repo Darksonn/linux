7a351bae407a0bd714d84c8cde1cda97c5acce32
Alice Ryhl <aliceryhl@google.com>
2023-07-24 11:43:49 +0000

    rust_binder: add TF_UPDATE_TXN support
    When a process is frozen, incoming oneway transactions are held in a
    queue until the process is unfrozen. If many oneway transactions are
    sent, then the process could run out of space for them. This patch adds
    a flag that avoids this by replacing previous oneway transactions in the
    queue to avoid having transactions of the same type build up. This can
    be useful when only the most recent transaction is necessary.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/defs.rs
+++ drivers/android/defs.rs
@@ -58,7 +58,13 @@ pub_no_prefix!(
 pub(crate) const FLAT_BINDER_FLAG_ACCEPTS_FDS: u32 = kernel::bindings::FLAT_BINDER_FLAG_ACCEPTS_FDS;
 pub(crate) const FLAT_BINDER_FLAG_TXN_SECURITY_CTX: u32 =
     kernel::bindings::FLAT_BINDER_FLAG_TXN_SECURITY_CTX;
-pub_no_prefix!(transaction_flags_, TF_ONE_WAY, TF_ACCEPT_FDS, TF_CLEAR_BUF);
+pub_no_prefix!(
+    transaction_flags_,
+    TF_ONE_WAY,
+    TF_ACCEPT_FDS,
+    TF_CLEAR_BUF,
+    TF_UPDATE_TXN
+);
 
 pub(crate) use bindings::{
     BINDER_TYPE_BINDER, BINDER_TYPE_FD, BINDER_TYPE_FDA, BINDER_TYPE_HANDLE, BINDER_TYPE_PTR,
